df54d963ad4793f1572f7b87be591d688d0ac6cd
update site <action@github.com>
2020-07-04 01:58:02 +0000

    Updated site

--- docs/en.search.min.2945dcaed5661fbf607fbdaf3effa3e271a814bced94c0292a526fa975945d49.js
+++ docs/en.search.min.2945dcaed5661fbf607fbdaf3effa3e271a814bced94c0292a526fa975945d49.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/symbiosisnovel/flexsearch.min.js');loadScript('/symbiosisnovel/en.search-data.min.a6120641367d055ff35f8c3622365c16279c8cf3aa1bda27bab258b3015276ac.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/symbiosisnovel/flexsearch.min.js');loadScript('/symbiosisnovel/en.search-data.min.2974c74d3f36880f7df7e0a4248ac78dd6c33c525073e43d4e1661757f01a1e1.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
